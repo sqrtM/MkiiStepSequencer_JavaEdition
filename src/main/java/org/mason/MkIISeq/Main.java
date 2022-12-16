3c42b53e6851bf970d8238a2d6bf1047765f4f8f
Mason Pike <masonapike@gmail.com>
2022-12-16 15:20:03 +0100

    now can recieve messages to control lights (barely)

--- src/main/java/org/mason/MkIISeq/Main.java
+++ src/main/java/org/mason/MkIISeq/Main.java
@@ -11,17 +11,30 @@ import static javax.sound.midi.MidiSystem.getMidiDevice;
 public class Main {
     public static void main(String[] args) throws MidiUnavailableException, InvalidMidiDataException {
 
+        // get list of all midi ports
         MidiDevice.Info[] allMidiDeviceInfo = findMidiDevices();
-        MidiDevice selectedDevice = selectMidiDevice(allMidiDeviceInfo);
-        Receiver selectedReceiver =  openMidiDevice(selectedDevice);
 
-        SequencerBank Mkii = new SequencerBank(selectedReceiver);
+        // select midiIn, attempt to open the port, then open the receiver
+        MidiDevice selectedMidiIn = selectMidiIn(allMidiDeviceInfo);
+        openMidiDevice(selectedMidiIn);
+        Receiver selectedReceiver =  openDeviceReceiver(selectedMidiIn);
+
+        // select midiOut, attempt to open the port, then open the transmitter
+        MidiDevice selectedMidiOut = selectMidiOut(allMidiDeviceInfo);
+        openMidiDevice(selectedMidiOut);
+        Transmitter selectedTransmitter = openDeviceTransmitter(selectedMidiOut);
+
+        System.out.println("Ports configured. Initializing sequencer...");
+
+        selectedTransmitter.setReceiver(new MidiInputReceiver());
+
+        SequencerBank Mkii = new SequencerBank(selectedReceiver, selectedTransmitter);
         Mkii.mainLoop();
     }
 
     private static MidiDevice.Info[] findMidiDevices()  {
 
-        Vector<Object> midiDeviceInfo = new Vector<>();
+        Vector<MidiDevice.Info> midiDeviceInfo = new Vector<>();
         MidiDevice.Info[] allMidiDeviceInfo = MidiSystem.getMidiDeviceInfo();
 
         for (int i = 0; i < allMidiDeviceInfo.length; i++) {
@@ -31,24 +44,46 @@ public class Main {
         return allMidiDeviceInfo;
     }
 
-    private static MidiDevice selectMidiDevice(MidiDevice.Info[] allMidiDeviceInfo) throws MidiUnavailableException {
+    private static MidiDevice selectMidiIn(MidiDevice.Info[] allMidiDeviceInfo) throws MidiUnavailableException {
 
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Please select a valid MIDI Device from the list above.");
+        System.out.println("Please select a valid MIDI IN port from the list above.");
         MidiDevice.Info selectedDeviceInfo = allMidiDeviceInfo[parseInt(in.nextLine())];
         System.out.println(selectedDeviceInfo + " selected. Connecting...");
         return getMidiDevice(selectedDeviceInfo);
     }
 
-    private static Receiver openMidiDevice(MidiDevice selectedDevice) throws MidiUnavailableException {
+    private static MidiDevice selectMidiOut(MidiDevice.Info[] allMidiDeviceInfo) throws MidiUnavailableException {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please select a valid MIDI Out port from the list above.");
+        MidiDevice.Info selectedDeviceInfo = allMidiDeviceInfo[parseInt(in.nextLine())];
+        System.out.println(selectedDeviceInfo + " selected. Connecting...");
+        return getMidiDevice(selectedDeviceInfo);
+    }
+
+    private static void openMidiDevice(MidiDevice selectedDevice) throws MidiUnavailableException {
 
         if (!(selectedDevice).isOpen()) {
             try {
+                System.out.println("Attempting to open " + selectedDevice.getDeviceInfo() + "...");
                 selectedDevice.open();
-                System.out.println(selectedDevice.getDeviceInfo() + " successfully connected.");
-            } catch (MidiUnavailableException e) { throw new MidiUnavailableException(); }
+                System.out.println("Success.");
+            } catch (MidiUnavailableException e) {
+                throw new MidiUnavailableException();
+            }
         }
-        return selectedDevice.getReceiver();
+    }
+
+    private static Receiver openDeviceReceiver(MidiDevice selectedDevice) throws MidiUnavailableException {
+        Receiver selectedReceiver = selectedDevice.getReceiver();
+        return selectedReceiver;
+    }
+
+    private static Transmitter openDeviceTransmitter(MidiDevice selectedDevice) throws MidiUnavailableException {
+        Transmitter selectedTransmitter = selectedDevice.getTransmitter();
+        return selectedTransmitter;
     }
 }
--- src/main/java/org/mason/MkIISeq/SequencerBank.java
+++ src/main/java/org/mason/MkIISeq/SequencerBank.java
@@ -6,20 +6,26 @@ import java.util.Arrays;
 public class SequencerBank {
 
     // 8 is a placeholder; this will be mutable
-    private static int bankLength = 8;
+    private static final int bankLength = 8;
     private final int BANK_ID = 0;
-    private final int MEMORY_BANKS = 16 - bankLength;
-    private int[][] totalMemory = new int[bankLength][MEMORY_BANKS];
+    private static final int MEMORY_BANKS = 16 - bankLength;
+    // make this 2D later. currently 1D for testing purposes.
+    private static final int[] totalMemory = new int[bankLength];
 
-    private final Receiver selectedDevice;
+    private final static byte HEX_OFFSET = 112;
+    private final static int NOTE_OFFSET = 36;
 
-    private final int PAD_ADDRESS = 9;
-    private final int PAD_COLOR   = 10;
+    private static Receiver selectedReceiver;
+    private static Transmitter selectedTransmitter;
 
-    private final int INACTIVE_OFF = -1;
-    private final int INACTIVE_ON  = 0;
-    private final int ACTIVE_OFF   = 1;
-    private final int ACTIVE_ON    = 2;
+
+    private static final int PAD_ADDRESS =  9;
+    private static final int PAD_COLOR   = 10;
+
+    private static final int INACTIVE_OFF = -1;
+    private static final int INACTIVE_ON  =  0;
+    private static final int ACTIVE_OFF   =  1;
+    private static final int ACTIVE_ON    =  2;
 
     private final static byte COLOR_NONE   = 0x00;
     private final static byte COLOR_RED    = 0x01;
@@ -30,36 +36,40 @@ public class SequencerBank {
     private final static byte COLOR_PURPLE = 0x11;
     private final static byte COLOR_WHITE  = 0x7F;
 
-    private byte inactiveOffColor;
-    private byte inactiveOnColor;
-    private byte activeOnColor;
-    private byte activeOffColor;
-    private byte bankColor;
+    private static byte inactiveOffColor;
+    private static byte inactiveOnColor;
+    private static byte activeOnColor;
+    private static byte activeOffColor;
+    private static byte bankColor;
 
+    private static final byte[] mkiiDefaultSysexMessage = {
+            (byte) 0xF0, 0x00, 0x20, 0x6B, 0x7F, 0x42, 0x02, 0x00, 0x10, 0x70, 0x14, (byte) 0xF7
+    };
 
-    public SequencerBank(Receiver selectedDevice, byte inactiveOffColor, byte inactiveOnColor, byte activeOnColor, byte activeOffColor, byte bankColor) {
-        this.selectedDevice = selectedDevice;
+    public SequencerBank(Receiver selectedReceiver, Transmitter selectedTransmitter, byte inactiveOffColor, byte inactiveOnColor, byte activeOnColor, byte activeOffColor, byte bankColor) {
+        SequencerBank.selectedReceiver = selectedReceiver;
+        SequencerBank.selectedTransmitter = selectedTransmitter;
 
-
-        this.inactiveOffColor = inactiveOffColor;
-        this.inactiveOnColor = inactiveOnColor;
-        this.activeOnColor = activeOnColor;
-        this.activeOffColor = activeOffColor;
-        this.bankColor = bankColor;
+        SequencerBank.inactiveOffColor = inactiveOffColor;
+        SequencerBank.inactiveOnColor = inactiveOnColor;
+        SequencerBank.activeOnColor = activeOnColor;
+        SequencerBank.activeOffColor = activeOffColor;
+        SequencerBank.bankColor = bankColor;
     }
 
-    public SequencerBank(Receiver selectedDevice) {
-        this(selectedDevice, COLOR_CYAN, COLOR_PURPLE, COLOR_WHITE, COLOR_GREEN, COLOR_BLUE);
+    public SequencerBank(Receiver selectedReceiver, Transmitter selectedTransmitter) {
+        this(selectedReceiver, selectedTransmitter, COLOR_CYAN, COLOR_PURPLE, COLOR_WHITE, COLOR_GREEN, COLOR_BLUE);
     }
 
     public SequencerBank() {
         throw new IllegalStateException("no midi device selected.");
     }
 
+
     // sleep is just for debugging. not permanent.
     public void mainLoop() throws InvalidMidiDataException {
         int beat = 0;
-        while (selectedDevice != null) {
+        while (selectedReceiver != null) {
             try {
                 Thread.sleep(1000);
                 buildMessage(beat);
@@ -67,37 +77,39 @@ public class SequencerBank {
                 System.out.println("InterruptedException Exception" + e.getMessage());
             }
             beat++;
-            if (beat > 8) { beat = 0; }
+            if (beat >= bankLength) { beat = 0; }
         }
     }
 
-    private final byte[] mkiiDefaultSysexMessage = {
-            (byte) 0xF0, 0x00, 0x20, 0x6B, 0x7F, 0x42, 0x02, 0x00, 0x10, 0x70, 0x14, (byte) 0xF7
-    };
+    // this is sending like 6 times in a row for some reason.
+    protected static void parseIncomingMessage(int padNumber) throws InvalidMidiDataException {
+        byte targetPad = (byte) (HEX_OFFSET + (padNumber - NOTE_OFFSET));
+        System.out.println(Arrays.toString(totalMemory));
+        byte[] outgoingMessage = mkiiDefaultSysexMessage.clone();
+        outgoingMessage[PAD_COLOR] = activeOnColor; outgoingMessage[PAD_ADDRESS] = targetPad;
+        sendMessage(outgoingMessage);
+        totalMemory[padNumber - NOTE_OFFSET] += 1;
+    }
 
     private void buildMessage(int beat) throws InvalidMidiDataException {
-        byte locationInBank = (byte) (112 + beat);
+        byte locationInBank = (byte) (HEX_OFFSET + beat);
         byte[] outgoingMessage = mkiiDefaultSysexMessage.clone();
 
         for (int pad = 0; pad < bankLength; pad++) {
-            switch (totalMemory[BANK_ID][pad]) {
+            switch (totalMemory[pad]) {
                 case INACTIVE_OFF -> {
-                    System.out.println("inactive off");
                     outgoingMessage[PAD_COLOR] = inactiveOffColor; outgoingMessage[PAD_ADDRESS] = locationInBank;
                     sendMessage(outgoingMessage);
                 }
                 case INACTIVE_ON -> {
-                    System.out.println("inactive on");
                     outgoingMessage[PAD_COLOR] = inactiveOnColor; outgoingMessage[PAD_ADDRESS] = locationInBank;
                     sendMessage(outgoingMessage);
                 }
                 case ACTIVE_OFF -> {
-                    System.out.println("active off");
                     outgoingMessage[PAD_COLOR] = activeOffColor; outgoingMessage[PAD_ADDRESS] = locationInBank;
                     sendMessage(outgoingMessage);
                 }
                 case ACTIVE_ON -> {
-                    System.out.println("active on");
                     outgoingMessage[PAD_COLOR] = activeOnColor; outgoingMessage[PAD_ADDRESS] = locationInBank;
                     sendMessage(outgoingMessage);
                 }
@@ -107,10 +119,9 @@ public class SequencerBank {
         System.out.println(beat);
     }
 
-    private void sendMessage(byte[] message) throws InvalidMidiDataException {
+    private static void sendMessage(byte[] message) throws InvalidMidiDataException {
         SysexMessage finalMessage = new SysexMessage();
         finalMessage.setMessage(message, 12);
-        selectedDevice.send(finalMessage, -1);
-        System.out.println(Arrays.toString(message));
+        selectedReceiver.send(finalMessage, -1);
     }
 }
